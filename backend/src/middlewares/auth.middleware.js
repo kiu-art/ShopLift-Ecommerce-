7b07d8abc854f7ea113a5a40e4eb1c959f86d623
kiu-art <kiusinh098@gmail.com>
2026-06-22 16:01:13 +0530

    Add authentication middleware to handle user login and token management

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.models";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/apiError";
+
+
+const isLoggedIn = async function(req,res,next){
+    const accessToken = req.cookies?.accessToken
+    if(accessToken){
+        const {_id} = await jwt.verify(accessToken , process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(_id);
+        if(!user){throw new ApiError(401,"Cannot be Authorized !!!")};
+        req.user = user;
+        next();
+    }
+    else{
+        const refreshToken = req.cookies?.refreshToken;
+        if(refreshToken){
+            const {_id} = await jwt.verify(refreshToken , process.env.REFRESH_TOKEN_SECRET);
+            const user = await User.findById(_id);
+            if(!user){throw new ApiError(401,"Cannot be Authorized !!!")};
+            req.user = user;
+            accessToken = user.generateAccessToken();
+            res.cookie("accessToken",accessToken);
+            next();
+        }
+        else{
+            throw new ApiError(401,"Cannot be Authorized !!!")
+        }
+    }
+}
+
+export {isLoggedIn};
